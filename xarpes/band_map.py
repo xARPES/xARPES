e078e7335fe0b04b454fff22351f5125a21d872d
Jan Berges <jan.berges@uni-bremen.de>
2024-05-29 16:14:15 +0200

    Fix warning about wrong underline length

--- xarpes/band_map.py
+++ xarpes/band_map.py
@@ -61,7 +61,7 @@ class band_map():
         Plots the band map.
 
         Returns
-        ----------
+        -------
         Matplotlib-Figure
         """
         from xarpes.functions import fit_leastsq
